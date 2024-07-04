d46897dc01dcaf9ad456fe24b92b851df64d0c32
samins <sarwar347422@gmail.com>
2024-07-03 22:52:19 -0400

    switch to vader more accurate sentiment analysis, add global variables to get a sentiment ratio of all reviews (positive/negative), run sentiment analysis on all pages of the review

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from textblob import TextBlob
 from transformers import pipeline
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from transformers import pipeline
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-sentiment_pipeline = pipeline("sentiment-analysis")
+sid_obj = SentimentIntensityAnalyzer()
 total_negative = 0
 total_positive = 0
 
@@ -37,19 +37,40 @@ def analyze_reviewpage(soup):
 
 def analyze_reviews(url):   
     # go from range 1 to 30, 30 will the maximum number of pages we look at for a general idea of customers's opinions
-    for x in range(1,30): 
+    for x in range(1,999): 
         soup = get_soup(url,custom_headers)
         analyze_reviewpage(soup)
         # if there exists no 'next page' button on the page then stop looking for reviews, because that means there are no more reviews to look for after looking at the current page
         if soup.find('li', {'class': 'a-disabled a-last'}):
             break
+    print("Total sentiment ratio: ", get_sentimentratio())
+    if(get_sentimentratio()>.65):
+        print("This product is receiving positive feedback from customers!")
 
 def get_sentiment(review):
+    #review_negative = 0 # number of sentences that are negative in the review
+    #review_positive = 0 # number of sentences that are positive in the review 
+    #review_neutral = 0
+    global total_positive
+    global total_negative
     review_sentences = review.split('.')
     for sentence in review_sentences:
-        data = [sentence]
-        sentiment_info = sentiment_pipeline(data)
-        print(sentiment_info[0]["label"], ' ', sentiment_info[0]["score"])
+        sentiment_dict = sid_obj.polarity_scores(sentence)
+        if sentiment_dict['compound'] >= 0.05 :
+            total_positive+=1
+            #review_positive+=1
+
+        elif sentiment_dict['compound'] <= - 0.05 :
+            total_negative+=1
+            #review_negative+=1
+    #print('positive: ', review_positive)
+    #print('negative: ', review_negative)
+    #print('neutral: ', review_neutral)
+
+def get_sentimentratio():
+    return (total_positive*1.0)/(total_negative+total_positive)
+
+#print(sentiment_info[0]["label"], ' ', sentiment_info[0]["score"])
 
 # data = [r_content_element]
 # print(sentiment_pipeline(data))
